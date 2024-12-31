2c12c30eb43aacfb32d775479011285ab90729b1
nickmarietta <nick.marietta@csu.fullerton.edu>
2024-12-30 23:32:00 -0800

    example page

--- LineupList/src/routes/+page.svelte
+++ LineupList/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { on } from "svelte/events";
+
+    let count = 0;
+</script>
+
+<h1>Counter: {count}</h1>
+<button on:click={() => count += 1}>Increment</button>
+<button on:click={() => count -= 1}>Decrement</button>
+<button on:click={() => count *= 3}>Multiply by 3</button>
+<button on:click={() => count = 0}>Reset</button>
